3a581b16df38870b1ffbf4bbadcf4249d9271ec4
dave hillman <davehillman@daves-MacBook-Pro.local>
2021-11-10 23:46:12 +0000

    built and ready to deploy

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dayCount = document.querySelector('#day');
+const hourCount = document.querySelector('#hour');
+const minCount = document.querySelector('#min');
+const secCount = document.querySelector('#sec');
+
+var date = new Date('Dec 18, 2021 00:00:00').getTime();
+
+var myfunc = setInterval(function() {
+    var timeNow = new Date().getTime();
+    var timeleft = date - timeNow;
+
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    dayCount.innerText = days;
+    hourCount.innerText = ('0' + hours).slice(-2);
+    minCount.innerText = ('0' + minutes).slice(-2);
+    secCount.innerText = ('0' + seconds).slice(-2);
+}, 1000);
